9e375e63811d3f7e61148737db13515e1c153de4
Akshay Navale <akshaynawale@gmail.com>
2025-08-23 18:19:09 +0100

    PhotoOrganizer: Basic thing working

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-console.log("Hello from Electron")
+import { app, BrowserWindow } from "electron";
+
+let createWindow = () => {
+    const Bwin = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
+
+    Bwin.loadFile("index.html");
+}
+
+// disabling hardware accerlation so we dont get warning like below
+// GetVSyncParametersIfAvailable() failed for 1 times
+app.disableHardwareAcceleration();
+
+app.whenReady().then(() => {
+    console.log("app is ready so creating window");
+    createWindow();
+})

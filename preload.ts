99f462eda0ad048f40bb362f402d38b840ecfd2d
Akshay Navale <akshaynawale@gmail.com>
2025-08-24 18:35:25 +0100

    PhotoOrg: Adding support to send logging from server to renderer
    1. the idea here is to send the logs form the server to the renderer.
    2. so when the server is trying to go over the photos we can send the
       logging information to the users to they can see what is the server
    doing under the hood.
    3. This should also help in the tshooting problems

--- preload.ts
+++ preload.ts
@@ -3,5 +3,13 @@ import { contextBridge, ipcRenderer } from 'electron';
 // expose only the file open dir read block
 contextBridge.exposeInMainWorld('electronAPI', {
     // the renderer will call this function which will securely send a message to the main process of electron
-    selectFolder: () => ipcRenderer.invoke('dialog:openDirectory')
+    selectFolder: () => ipcRenderer.invoke('dialog:openDirectory'),
+    handleLogMessage: (callback: (msg: {message: string, level: string}) => void ) => {
+        ipcRenderer.on(
+            'send-to-frontend-channel', (_event, msg) => {
+                console.log("inside the preload script handleLogMessage function");
+                callback(msg);
+            }
+        );
+    }
 });
